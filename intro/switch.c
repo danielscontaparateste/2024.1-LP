0db615667e93784042b53700263dbd22060af02b
Daniel Ferreira <danielse@ifce.edu.br>
2024-02-29 11:48:22 -0300

    Aula 28/02

---
+++ intro/switch.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+    int x, y, res;
+    puts("Entre com o primeiro valor: ");
+    scanf("%d%*c",&x); //Ver: https://faq.cprogramming.com/cgi-bin/smartfaq.cgi?answer=1352443831&id=1043284392
+    puts("Entre com o segundo valor: ");
+    scanf("%d%*c",&y);
+
+    puts("Digite: ");
+    puts("1. Somar");
+    puts("2. Subtrair");
+
+    char op = getchar();
+
+    printf("op : %d\n",op);
+
+    switch(op){
+        case '1':
+            res = x + y;
+        break;
+        case '2':
+            res = x - y;
+        break;
+        default:
+            puts("Opção Inválida");        
+    }
+
+    printf("Resposta: %d\n", res);
+
+    return 0;
+
+}
